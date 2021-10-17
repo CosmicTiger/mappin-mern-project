09bf891a766e720b4c9b3b451cffe6b83793755b
CosmicTiger <luisangelmarcia@gmail.com>
2021-10-16 20:16:07 -0600

    :sparkles: - feature: Done fetching data from backend using axios and popups well made

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 import RoomIcon from '@mui/icons-material/Room';
 import StarIcon from '@mui/icons-material/Star';
+import axios from 'axios';
+import { format } from 'timeago.js';
 
 import './App.css';
 
 function App() {
+  const currentUser = "jane";
+  const [pins, setPins] = useState([]);
+  const [currentPlaceId, setCurrentPlaceId] = useState(null);
+  const [newPlace, setNewPlace] = useState(null);
   const [viewport, setViewport] = useState({
     width: '100vw',
     height: '100vh',
@@ -14,7 +20,41 @@ function App() {
     zoom: 4
   });
 
+  const fillingOptions = () => {
+    const rowsOptions = [];
+    for (let i = 1; i <= 5; i++) {
+      rowsOptions.push(<option key={i} value={i}>{i}</option>)
+    }
+
+    return rowsOptions
+  }
+
   const handlingMapOnChange = nextViewport => setViewport(nextViewport);
+  const handleMarkerClick = (id, latitude, longitude) => {
+    setCurrentPlaceId(id);
+    setViewport({ ...viewport, latitude, longitude });
+  }
+
+  const handleAddPinClick = e => {
+    const [longitude, latitude] = e.lngLat;
+    setNewPlace({
+      latitude,
+      longitude
+    });
+  }
+
+  useEffect(() => {
+    const getPins = async () => {
+      try {
+        const response = await axios.get('/pins');
+        setPins(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    getPins();
+  }, [])
 
   return (
     <div className="App">
@@ -22,37 +62,76 @@ function App() {
         {...viewport}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
         onViewportChange={handlingMapOnChange}
-        mapStyle="mapbox://styles/jokerbyakko/ckurjdfqr0euz15o0wnzx3yyh"
+        mapStyle={process.env.REACT_APP_MAPBOX_STYLE}
+        onDblClick={handleAddPinClick}
+        transitionDuration="200"
       >
-        <Marker latitude={48.858093} longitude={2.294694} offsetLeft={-20} offsetTop={-10}>
-          <RoomIcon
-            style={{ fontSize: viewport.zoom * 7, color: 'slateblue' }}
-          />
-        </Marker>
-        {/* <Popup
-          latitude={48.858093}
-          longitude={2.294694}
-          closeButton={true}
-          closeOnClick={false}
-          anchor="left" >
-          <div className="Card">
-            <label>Place</label>
-            <h4 className="Card-placeName">Eiffell Tower</h4>
-            <label>Review</label>
-            <p className="Card-description">Beautiful place. I like it.</p>
-            <label>Rating</label>
-            <div className="Card-rating">
-              <StarIcon className="Card-ratingIcon" />
-              <StarIcon className="Card-ratingIcon" />
-              <StarIcon className="Card-ratingIcon" />
-              <StarIcon className="Card-ratingIcon" />
-              <StarIcon className="Card-ratingIcon" />
+        {
+          pins && pins.map(pin => (
+            <>
+              <Marker latitude={pin.latitude} longitude={pin.longitude} offsetLeft={-20} offsetTop={-10}>
+                <RoomIcon
+                  style={{
+                    fontSize: viewport.zoom * 7,
+                    color: pin.username === currentUser ? 'tomato' : 'slateblue',
+                    cursor: "pointer"
+                  }}
+                  onClick={() => handleMarkerClick(pin._id, pin.latitude, pin.longitude)}
+                />
+              </Marker>
+              {pin._id === currentPlaceId &&
+                <Popup
+                  latitude={pin.latitude}
+                  longitude={pin.longitude}
+                  closeButton={true}
+                  closeOnClick={false}
+                  onClose={() => setCurrentPlaceId(null)}
+                  anchor="left">
+                  <div className="Card">
+                    <label>Place</label>
+                    <h4 className="Card-placeName">{pin.title}</h4>
+                    <label>Review</label>
+                    <p className="Card-description">{pin.description}</p>
+                    <label>Rating</label>
+                    <div className="Card-rating">
+                      <StarIcon className="Card-ratingIcon" />
+                      <StarIcon className="Card-ratingIcon" />
+                      <StarIcon className="Card-ratingIcon" />
+                      <StarIcon className="Card-ratingIcon" />
+                      <StarIcon className="Card-ratingIcon" />
+                    </div>
+                    <label>Information</label>
+                    <span className="Card-usernameAuthor"> Created by <b>{pin.username}</b></span>
+                    <span className="Card-publicationDate">{format(pin.createdAt)}</span>
+                  </div>
+                </Popup>
+              }
+            </>
+          ))
+        }
+        {newPlace && (
+          <Popup
+            latitude={newPlace.latitude}
+            longitude={newPlace.longitude}
+            closeButton={true}
+            closeOnClick={false}
+            onClose={() => setNewPlace(null)}
+            anchor="left">
+            <div>
+              <form>
+                <label>Title</label>
+                <input placeholder="Enter a title" type="text" name="title" />
+                <label>Review</label>
+                <input placeholder="Say your opinions about this place" type="text" name="review" />
+                <label>Rating</label>
+                <select>
+                  {fillingOptions()}
+                </select>
+                <button className="submitButton" type="submit">Add Pin</button>
+              </form>
             </div>
-            <label>Information</label>
-            <span className="Card-usernameAuthor"> Created by <b>CosmicTiger</b></span>
-            <span className="Card-publicationDate">1 hour ago</span>
-          </div>
-        </Popup> */}
+          </Popup>
+        )}
       </ReactMapGL>
     </div>
   );
